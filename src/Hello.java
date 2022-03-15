39cd06af03542b218a82c30f991336aac5e578e4
iyangming <wuyangming@szkingdom.com>
2022-03-15 12:29:46 +0800

    modified hello.java

--- src/Hello.java
+++ src/Hello.java
@@ -2,5 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.printf("helo");
         System.out.printf("world");
+        System.out.printf("testing");
+
     }
 }
